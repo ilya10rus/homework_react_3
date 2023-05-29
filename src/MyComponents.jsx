4cd3b6b4d61fc805a9942173bd9e51b26afa0913
Ilya Petrenko <ilya.petrenko.96@mail.ru>
2023-05-29 21:15:44 +0300

    homework_2_com

--- src/MyComponents.jsx
+++ src/MyComponents.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operators = ['+', '-', '=', 'C'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '=', 'C'];
 
 export const MyComponent = () => {
 	const [value, setValue] = useState(0);
@@ -18,32 +17,28 @@ export const MyComponent = () => {
 		} else if (operator) {
 			setValue2((preVal2) => preVal2 + num);
 			setFinish(false);
-		} else {
+		} else if (value !== 0 && value !== '+' && value !== '-') {
 			setValue((preVal) => preVal + `${num}`);
 			setFinish(false);
 		}
-	};
-
-	const showOperator = (oper) => {
-		if (oper === 'C') {
-			setValue(0);
-			setValue2('');
-			setOperator('');
-			setFinish(false);
-		} else if (oper === '+') {
-			setOperator(oper);
-			setFinish(false);
-		} else if (oper === '-') {
-			setOperator(oper);
-			setFinish(false);
-		} else if (oper !== 'C' && oper !== '=') {
-			setOperator(oper);
-			setFinish(false);
-		} else if (oper === '=') {
-			setValue2('');
-			setOperator('');
-			setValue(eval(operands));
-			setFinish(true);
+		if (num === '+' || num === '-' || num === '=' || num === 'C') {
+			if (num === 'C') {
+				setValue(0);
+				setValue2('');
+				setOperator('');
+				setFinish(false);
+			} else if (num === '+') {
+				setFinish(false);
+			} else if (num === '-') {
+				setFinish(false);
+			} else if (num !== 'C' && num !== '=') {
+				setFinish(false);
+			} else if (num === '=') {
+				setValue2('');
+				setOperator('');
+				setValue(eval(operands));
+				setFinish(true);
+			}
 		}
 	};
 
@@ -55,15 +50,6 @@ export const MyComponent = () => {
 			{numbers.map((num) => (
 				<a className={styles.calcNum} key={num} onClick={() => showValue(num)}>
 					{num}
-				</a>
-			))}
-			{operators.map((oper) => (
-				<a
-					className={styles.calcNum}
-					key={oper}
-					onClick={() => showOperator(oper)}
-				>
-					{oper}
 				</a>
 			))}
 		</div>
